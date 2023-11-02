ef6cd676269605083d61f796bf8965b9a1df3b93
SelmaAtasert <selmaatasert1@gmail.com>
2023-11-02 08:49:34 +0100

    created folder structure and components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import {NavLink, Link} from "react-router-dom"
+import Cart from "./Cart"
+
+export default function NavBar() {
+  return (
+    <>
+    <div className="header">
+        <Link to="/" className="nav-icon"></Link>
+        <nav>
+            <ul>
+                <li><NavLink to="/products"><span>Men's Clothing</span></NavLink></li>
+                <li><NavLink to="/products"><span>Women's Clothing</span></NavLink></li>
+                <li><NavLink to="/products"><span>Electronics</span></NavLink></li>
+                <li><NavLink to="/products"><span>Jewelry</span></NavLink></li>
+            </ul>
+        </nav>
+        <Cart/>
+        
+    </div>
+    </>
+  )
+}
---
+++ src/views/ProductPage.jsx
@@ -0,0 +1,11 @@
+import Products from '../components/Products'
+import SideBar from '../components/SideBar'
+
+export default function ProductList() {
+  return (
+    <div>
+        <SideBar/>
+        <Products/>
+    </div>
+  )
+}
