7686c8715855ac03893aa9296dc1b12f6a883e93
Pavithra <pavithra.narayanan.kutty@gmail.com>
2023-11-02 14:59:18 +0100

    h1 added

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <h1>Vogue Valley</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<SingleProduct />} />
