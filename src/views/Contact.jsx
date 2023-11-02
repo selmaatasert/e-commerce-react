1dbd216943de0ef9c7529d831a4873fc3515163d
didier <rubangisadidier@gmail.com>
2023-11-02 21:57:33 +0100

    Some changes

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export default function Contact() {
-  return (
-    <div className='contact-page'>
-      Contact
-    </div>
-  )
-}
+const Contact = () => {
+  return <div className="contact-page">Contact</div>;
+};
+
+export default Contact;
