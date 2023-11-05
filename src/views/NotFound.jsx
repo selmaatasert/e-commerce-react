3cb197436bbc21b2f05c76ad95f4fb23513ed1ce
Ilya <zubrik1984@yahoo.com>
2023-11-06 00:27:19 +0100

    footer+notfound+about

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+// import notFoundImage from '../images/not-found.png'; 
 
-export default function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
+import '../styles/NotFound.css'; 
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div className="not-found">
+        <h1>We looked everywhere.</h1>
+        <p className="large-text">Looks like this page is missing.</p>
+        <p className="small-text">
+          If you still need help, visit our help pages.
+        </p>
+        <Link to="/" className="home-button">Go to homepage</Link>
+        {/* <img src={notFoundImage} alt="Not Found" className="not-found-image" /> */}
+      </div>
+    );
+  }
 }
+
+export default NotFound;
