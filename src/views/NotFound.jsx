edcc947ac70b057c38958c7894dc326779c4c4fa
Ilya <zubrik1984@yahoo.com>
2023-11-06 13:37:02 +0100

    added classes

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -8,7 +8,7 @@ class NotFound extends React.Component {
   render() {
     return (
       <div className="not-found">
-        <h1>We looked everywhere.</h1>
+        <h1 className='h1-404'>We looked everywhere.</h1>
         <p className="large-text">Looks like this page is missing.</p>
         <p className="small-text">
           If you still need help, visit our help pages.
