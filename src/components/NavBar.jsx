1e2bbb3a4b2e2ad1064199050d7fc910abe8d347
Pavithra <pavithra.narayanan.kutty@gmail.com>
2023-11-06 17:28:04 +0100

    styling update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,8 @@ import { NavLink, Link } from "react-router-dom";
 import { CategoryFilterContext } from "../context/AppContexts";
 import Cart from "./Cart";
 import "./NavBar.scss";
+//
+import Logo from "./assets/3.png";
 
 export default function NavBar() {
   const { dispatch } = useContext(CategoryFilterContext);
@@ -15,18 +17,18 @@ export default function NavBar() {
     <>
       <div className="header">
         <Link to="/" className="nav-header">
-          Vogue Valley
+          <img src={Logo} alt="logo" />
         </Link>
         <nav className="header-nav">
           <ul>
             <li>
               <NavLink onClick={handleCategoryClick} to="/products">
-                <span id="men">Men's Clothing</span>
+                <span id="men">Men</span>
               </NavLink>
             </li>
             <li>
               <NavLink onClick={handleCategoryClick} to="/products">
-                <span id="women">Women's Clothing</span>
+                <span id="women">Women</span>
               </NavLink>
             </li>
             <li>
@@ -40,7 +42,7 @@ export default function NavBar() {
               </NavLink>
             </li>
             <li>
-              <NavLink  onClick={handleCategoryClick} to="/contact">
+              <NavLink onClick={handleCategoryClick} to="/contact">
                 <span>Contact</span>
               </NavLink>
             </li>
