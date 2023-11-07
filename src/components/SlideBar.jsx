cc6f23b60c9de9fe63d15470fbb9629edd84a810
Pavithra <pavithra.narayanan.kutty@gmail.com>
2023-11-07 09:22:04 +0100

    slider poster

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -5,7 +5,7 @@ import "./SlideBar.scss"
 export default function SlideBar() {
   return (
     <div  className='poster-div'>
-      <Link to="/products" className='poster-collection'>New Collection!</Link>
+      <Link to="/products" className='poster-collection'>New Winter Collection!</Link>
     </div>
   );
 }
